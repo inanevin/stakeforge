ad297015e7c3b51a4bd31695784bee81f3ed1fb2
Inan Evin <inanevin@gmail.com>
2026-05-14 10:10:00 +0300

    rectf

---
+++ stakeforge_core/src/sfg/math/rectf.cpp
@@ -0,0 +1,147 @@
+/*
+This file is a part of stakeforge_engine: https://github.com/inanevin/stakeforge
+Copyright [2025-] Inan Evin
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this
+	  list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#include "rectf.hpp"
+#include "math.hpp"
+#include "vec2f.hpp"
+#include <sfg/data/istream.hpp>
+#include <sfg/data/ostream.hpp>
+
+#include <sfg/vendor/nhlohmann/json.hpp>
+
+namespace sfg
+{
+	const rectf_t rectf_t::zero = {0.0f, 0.0f, 0.0f, 0.0f};
+	const rectf_t rectf_t::one	= {0.0f, 0.0f, 1.0f, 1.0f};
+
+	rectf_t rectf_t::from_min_max(const vec2f_t& min, const vec2f_t& max)
+	{
+		return {min.x, min.y, max.x - min.x, max.y - min.y};
+	}
+
+	rectf_t rectf_t::from_min_max(f32 min_x, f32 min_y, f32 max_x, f32 max_y)
+	{
+		return {min_x, min_y, max_x - min_x, max_y - min_y};
+	}
+
+	bool rectf_t::equals(const rectf_t& other, f32 epsilon) const
+	{
+		return math::almost_equal(x, other.x, epsilon) && math::almost_equal(y, other.y, epsilon) && math::almost_equal(w, other.w, epsilon) && math::almost_equal(h, other.h, epsilon);
+	}
+
+	bool rectf_t::is_zero(f32 epsilon) const
+	{
+		return math::almost_equal(x, 0.0f, epsilon) && math::almost_equal(y, 0.0f, epsilon) && math::almost_equal(w, 0.0f, epsilon) && math::almost_equal(h, 0.0f, epsilon);
+	}
+
+	bool rectf_t::is_point_inside(const vec2f_t& point) const
+	{
+		return is_point_inside(point.x, point.y);
+	}
+
+	bool rectf_t::is_point_inside(f32 px, f32 py) const
+	{
+		return px >= x && px <= get_right() && py >= y && py <= get_bottom();
+	}
+
+	bool rectf_t::is_overlapping(const rectf_t& other) const
+	{
+		return x < other.get_right() && get_right() > other.x && y < other.get_bottom() && get_bottom() > other.y;
+	}
+
+	bool rectf_t::is_inside(const rectf_t& other) const
+	{
+		return other.contains(*this);
+	}
+
+	bool rectf_t::contains(const rectf_t& other) const
+	{
+		return other.x >= x && other.get_right() <= get_right() && other.y >= y && other.get_bottom() <= get_bottom();
+	}
+
+	vec2f_t rectf_t::get_min() const
+	{
+		return {x, y};
+	}
+
+	vec2f_t rectf_t::get_max() const
+	{
+		return {get_right(), get_bottom()};
+	}
+
+	vec2f_t rectf_t::get_pos() const
+	{
+		return {x, y};
+	}
+
+	vec2f_t rectf_t::get_size() const
+	{
+		return {w, h};
+	}
+
+	f32 rectf_t::get_left() const
+	{
+		return x;
+	}
+
+	f32 rectf_t::get_right() const
+	{
+		return x + w;
+	}
+
+	f32 rectf_t::get_top() const
+	{
+		return y;
+	}
+
+	f32 rectf_t::get_bottom() const
+	{
+		return y + h;
+	}
+
+	void rectf_t::serialize(ostream_t& stream) const
+	{
+		stream << x << y << w << h;
+	}
+
+	void rectf_t::deserialize(istream_t& stream)
+	{
+		stream >> x >> y >> w >> h;
+	}
+
+	void to_json(nlohmann::json& j, const rectf_t& v)
+	{
+		j = nlohmann::json::array_t({v.x, v.y, v.w, v.h});
+	}
+
+	void from_json(const nlohmann::json& j, rectf_t& v)
+	{
+		v.x = j.at(0).get<f32>();
+		v.y = j.at(1).get<f32>();
+		v.w = j.at(2).get<f32>();
+		v.h = j.at(3).get<f32>();
+	}
+}
---
+++ stakeforge_core/src/sfg/math/rectf.hpp
@@ -0,0 +1,84 @@
+/*
+This file is a part of stakeforge_engine: https://github.com/inanevin/stakeforge
+Copyright [2025-] Inan Evin
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this
+	  list of conditions and the following disclaimer.
+
+   2. Redistributions in binary form must reproduce the above copyright notice,
+	  this list of conditions and the following disclaimer in the documentation
+	  and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#pragma once
+
+#include "math_common.hpp"
+#include <sfg/vendor/nhlohmann/json_fwd.hpp>
+
+namespace sfg
+{
+	class istream_t;
+	class ostream_t;
+
+	struct vec2f_t;
+
+	struct rectf_t
+	{
+		f32 x;
+		f32 y;
+		f32 w;
+		f32 h;
+
+		static const rectf_t zero;
+		static const rectf_t one;
+
+		static rectf_t from_min_max(const vec2f_t& min, const vec2f_t& max);
+		static rectf_t from_min_max(f32 min_x, f32 min_y, f32 max_x, f32 max_y);
+
+		bool equals(const rectf_t& other, f32 epsilon = MATH_EPS) const;
+		bool is_zero(f32 epsilon = MATH_EPS) const;
+		bool is_point_inside(const vec2f_t& point) const;
+		bool is_point_inside(f32 px, f32 py) const;
+		bool is_overlapping(const rectf_t& other) const;
+		bool is_inside(const rectf_t& other) const;
+		bool contains(const rectf_t& other) const;
+
+		vec2f_t get_min() const;
+		vec2f_t get_max() const;
+		vec2f_t get_pos() const;
+		vec2f_t get_size() const;
+		f32		get_left() const;
+		f32		get_right() const;
+		f32		get_top() const;
+		f32		get_bottom() const;
+
+		void serialize(ostream_t& stream) const;
+		void deserialize(istream_t& stream);
+
+		inline bool operator==(const rectf_t& other) const
+		{
+			return equals(other);
+		}
+		inline bool operator!=(const rectf_t& other) const
+		{
+			return !equals(other);
+		}
+	};
+
+	void to_json(nlohmann::json& j, const rectf_t& v);
+	void from_json(const nlohmann::json& j, rectf_t& v);
+}
